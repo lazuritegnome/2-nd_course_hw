a8262fe5b8901c001e3c030e45ef527a7257a076
lazuritegnome <gorohowit@gmail.com>
2023-08-24 23:05:14 +0300

    hw2

---
+++ hw-2.js
@@ -0,0 +1,139 @@
+//*задние 1
+
+let a = 10;
+alert(a);
+a = 20;
+alert(a)
+
+//* задание 2
+
+// let yearReleased = 2007;
+// alert(yearReleased);
+
+//* задание 3
+
+// let nameCreator = ('Брендан Эйх');
+// alert(nameCreator);
+
+//* задание 4
+
+// let a = 10;
+// let b = 2;
+// alert(`
+// Сумма: ${a + b}
+// Разность: ${a - b} 
+// Произведение: ${a * b}
+// Частное: ${a / b}
+// `);
+
+// or 
+
+// let a = 10;
+// let b = 2;
+// alert(`Сумма: ${a + b}`);
+// alert(`Разность: ${a - b}`);
+// alert(`Произведение: ${a * b}`);
+// alert(`Частное: ${a / b}`);
+
+//* задание 5
+
+// let a = 2**5;
+// let result = a;
+// alert(result);
+
+//* задание 6
+
+// let a = 9;
+// let b = 2;
+// let remainder = a % b;
+// alert(`Остаток деления ${a} на ${b} : ${remainder}`);
+
+// or
+
+// let a = 9;
+// let b = 2;
+// alert(`остаток от деления ${a} на ${b} : ${a % b}`);
+
+//* задание 7
+
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num ++;
+// num --;
+// alert(num);
+
+//* задание 8
+
+// let age = Number(prompt('Сколько вам лет?'));
+// alert(`Ваш возраст: ${age}`);
+
+//* Задание 9
+
+// let user ={
+//     name:'egor',
+//     age: 22,
+//     isAdmin: true
+
+//* Задание 9.1
+
+// let user ={
+//     name:'egor',
+//     age: 22,
+//     isAdmin: true,
+//     };
+//     user["city Of Residence"] = 'Moscow';
+//     alert(user["city Of Residence"]);
+
+// or
+
+// let user ={
+//     name:'egor',
+//     age: 22,
+//     isAdmin: true,
+//     'city Of Residence': 'Moscow'
+//     };
+//     alert(user["city Of Residence"]);
+    
+//* Задание 9.2
+
+// let user ={
+//     name:'egor',
+//     age: 22,
+//     isAdmin: true,
+//     };
+//     user["city Of Residence"] = 'Moscow';
+//     user.name = 'dmitry';
+//     alert(user.name);
+
+//* Задание 9.3
+
+// let user ={
+//     name:'egor',
+//     age: 22,
+//     isAdmin: true,
+//     };
+//     user["city Of Residence"] = 'Moscow';
+//     user.name = 'dmitry';
+//     delete user["city Of Residence"];
+//     alert(user["city Of Residence"]);
+
+//* Задание 9.4
+
+// let user ={
+//     name:'egor',
+//     age: 22,
+//     isAdmin: true,
+//     'city Of Residence': 'Moscow'
+//     };
+//     user.name = 'dmitry';
+//     delete user["city Of Residence"];
+// let info = prompt('Какую информацию хотите узнать о пользователе?');
+//     alert(user[info]);
+
+//* Задание 10
+
+// let userName = prompt('Как вас зовут?');
+// alert(`Привет! ${userName}!`);
